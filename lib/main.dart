85979b1078bbfc39b0698ec0da3829f4c35a1a80
vitch07 <vitchubalaji@gmail.com>
2024-02-03 12:22:29 +0530

    working_commit_v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:speech_to_text/speech_to_text.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:google_translate/google_translate.dart' as translate;
 
 void main() {
   runApp(MyApp());
@@ -41,14 +38,10 @@ class _SearchPageState extends State<SearchPage> {
     processedQuery = processedQuery.replaceAll(' ', '+');
     final Uri url = Uri.parse(
         'https://www.youtube.com/results?search_query=$processedQuery');
+    // Launch URL
     if (!await launchUrl(url)) {
       throw Exception('Could not launch $url');
     }
-  }
-
-  void _navigateToVoiceSearch() {
-    Navigator.of(context).push(MaterialPageRoute(
-        builder: (_) => VoiceSearchPage(translate.TranslateApi())));
   }
 
   @override
@@ -74,11 +67,6 @@ class _SearchPageState extends State<SearchPage> {
               onPressed: _searchYouTube,
               child: Text('Search on YouTube'),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _navigateToVoiceSearch,
-              child: Text('Voice Search'),
-            ),
           ],
         ),
       ),
@@ -86,116 +74,8 @@ class _SearchPageState extends State<SearchPage> {
   }
 }
 
-class VoiceSearchPage extends StatefulWidget {
-  final translate.Translate translateApi;
-
-  VoiceSearchPage(this.translateApi);
-
-  @override
-  _VoiceSearchPageState createState() => _VoiceSearchPageState();
-}
-
-class _VoiceSearchPageState extends State<VoiceSearchPage> {
-  final SpeechToText _speechToText = SpeechToText();
-  bool _speechEnabled = false;
-  String _lastWords = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _initSpeech();
-  }
-
-  void _initSpeech() async {
-    _speechEnabled = await _speechToText.initialize();
-    setState(() {});
-  }
-
-  void _startListening() async {
-    await _speechToText.listen(onResult: _onSpeechResult);
-    setState(() {});
-  }
-
-  void _stopListening() async {
-    await _speechToText.stop();
-    setState(() {});
-  }
-
-  void _onSpeechResult(SpeechRecognitionResult result) {
-    setState(() {
-      _lastWords = result.recognizedWords;
-    });
-    if (result.finalResult) {
-      _performSearch(_lastWords);
-    }
-  }
-
-  void _performSearch(String searchQuery) async {
-    // Translate recognized words to English
-    final translatedQuery = await widget.translateApi.translations.list(
-      [searchQuery],
-      source: 'auto', // Auto-detect source language
-      target: 'en', // Translate to English
-    );
-    final translatedText = translatedQuery.translations[0].translatedText;
-    // Construct YouTube search URL
-    final processedQuery = translatedText.replaceAll(' ', '+');
-    final Uri url = Uri.parse(
-        'https://www.youtube.com/results?search_query=$processedQuery');
-    // Launch URL
-    if (!await launchUrl(url)) {
-      throw Exception('Could not launch $url');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Voice Search'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                'Recognized words:',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  _speechToText.isListening
-                      ? _lastWords
-                      : _speechEnabled
-                          ? 'Tap the microphone to start listening...'
-                          : 'Speech not available',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:
-            _speechToText.isNotListening ? _startListening : _stopListening,
-        tooltip: 'Listen',
-        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
-      ),
-    );
-  }
-}
-
-Future<bool> launchUrl(Uri url) async {
-  if (await canLaunch(url.toString())) {
-    await launch(url.toString());
-    return true;
-  } else {
-    return false;
+Future<void> _launchUrl(Uri url) async {
+  if (!await launchUrl(url)) {
+    throw Exception('Could not launch $url');
   }
 }
